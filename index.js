5c2367de431523fecdcb390db13fbc6767377882
fizan-iqbal <se.fizaniqbal2@gmail.com>
2023-03-02 16:13:45 +0500

    uploading

--- index.js
+++ index.js
@@ -10,6 +10,15 @@ import messagesRoutes from "./routes/messagesRoute.js";
 dotenv.config();
 const app = express();
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
